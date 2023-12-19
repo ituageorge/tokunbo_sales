a5adc3e48eff05bbc238ae9185c397a418b1ffa8
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2023-12-19 12:30:36 +0100

    indicating on the client side the saving and the saved process

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,66 +1,94 @@
-'use client';
+"use client";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
-// useRouter
+
 export default function ProfilePage() {
-    const router = useRouter();
-    const session = useSession();
-    const [userName, setUserName] = useState(session?.data?.user?.name || '' )
-    let {status} = session;
-    // console.log('session4Profile', session?.data?.user?.image)
+  const router = useRouter();
+  const session = useSession();
+  const [userName, setUserName] = useState(session?.data?.user?.name || "");
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+  let { status } = session;
+  // console.log('session4Profile', session?.data?.user?.image)
 
-        useEffect(() => {
-            if(status === 'authenticated') {
-                setUserName(session?.data?.user?.name)
-            }
-        }, [session, status]);
-
-     const handleProfileInputUpdate = (e) => {
-        e.preventDefault();
-        fetch('/api/profile', {
-            method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({name: userName}),
-
-        })
+  useEffect(() => {
+    if (status === "authenticated") {
+      setUserName(session?.data?.user?.name);
     }
+  }, [session, status]);
 
-    if(status === 'loading') {
-        return 'Loading...'
+  const handleProfileInputUpdate = (e) => {
+    e.preventDefault();
+    setSaved(false);
+    setIsSaving(true)
+    fetch("/api/profile", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: userName }),
+    });
+      setIsSaving(false);
+
+    if (response.ok) {
+      setSaved(true);
     }
+  };
 
-    if(status === 'unauthenticated'){
-        router.push('/login');
-    }
+  if (status === "loading") {
+    return "Loading...";
+  }
 
-    const userImage = session?.data?.user?.image
-    return (
-        <section className='mt-8'>
-            <h1 className="text-center text-primary text-4xl mb-4">
-        Profile
-      </h1>
-      <div className='max-w-md mx-auto'>
+  if (status === "unauthenticated") {
+    router.push("/login");
+  }
+
+  const userImage = session?.data?.user?.image;
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
+      <div className="max-w-md mx-auto">
+        {saved && (
+          <h2 className="text-center bg-green-100 p-4 border-4 border-green-300 rounded-lg">
+            Profile saved!
+          </h2>
+        )}
+         {isSaving && (
+          <h2 className="text-center bg-blue-100 p-4 border-4 border-blue-300 rounded-lg">
+            Profile is Saving!
+          </h2>
+        )}
         <div className="flex gap-4 items-center">
-        <div>
+          <div>
             <div className="p-2 rounded-lg relative">
-                
-                    <Image className="rounded-lg h-full w-full mb-1" src={userImage} height={250} width={250} alt="avatar" />
+              <Image
+                className="rounded-lg h-full w-full mb-1"
+                src={userImage}
+                height={250}
+                width={250}
+                alt="avatar"
+              />
 
-    
-         <button type="button">Edit</button>
-         </div>
-        </div>
-        <form className="grow" onSubmit={handleProfileInputUpdate}>
-            <input type="text" placeholder=" First and last name" value={userName} onChange={e => setUserName(e.target.value) } />
-            <input type="email" disabled={true} value={session?.data?.user?.email}/>
+              <button type="button">Edit</button>
+            </div>
+          </div>
+          <form className="grow" onSubmit={handleProfileInputUpdate}>
+            <input
+              type="text"
+              placeholder=" First and last name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              type="email"
+              disabled={true}
+              value={session?.data?.user?.email}
+            />
             <button type="submit">Save</button>
-        </form>
+          </form>
         </div>
       </div>
-        </section>
-    )
+    </section>
+  );
 }
