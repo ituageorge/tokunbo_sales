ccd9870d64ba63d4d1cc2cd62e76a6f79a1d675c
Osagie <ituaosagie100@gmail.com>
2024-07-24 22:19:25 +0100

    nextauth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
-import clientPromise from "../../../../libs/mongoConnect";
-import { UserInfo } from "../../../models/UserInfo";
+import clientPromise from "../../../../libs/mongoConnect"; //"@/libs/mongoConnect"
+import {UserInfo} from "../../../models/UserInfo"; //@/models/UserInfo
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
-import { User } from "../../../models/User";
-import NextAuth from "next-auth";
+import {User} from '../../../models/User';
+import NextAuth, {getServerSession} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -20,33 +20,34 @@ export const authOptions = {
       name: 'Credentials',
       id: 'credentials',
       credentials: {
-        email: { label: "Email", type: "email", placeholder: "your_email_address.com" },
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
+      async authorize(credentials, req) {
         const email = credentials?.email;
         const password = credentials?.password;
 
-        await mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({ email });
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({email});
         const passwordOk = user && bcrypt.compareSync(password, user.password);
 
         if (passwordOk) {
           return user;
         }
 
-        return null;
+        return null
       }
     })
   ],
 };
 
-export async function isAdmin(session) {
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
   if (!userEmail) {
     return false;
   }
-  const userInfo = await UserInfo.findOne({ email: userEmail });
+  const userInfo = await UserInfo.findOne({email:userEmail});
   if (!userInfo) {
     return false;
   }
@@ -55,4 +56,4 @@ export async function isAdmin(session) {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
