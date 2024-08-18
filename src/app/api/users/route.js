de9b55695f0c6b801173086ab4af6c5aa75aaae3
Osagie <ituaosagie100@gmail.com>
2024-08-18 16:21:40 +0100

    newer update

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 
 export const GET = async() => {
     mongoose.connect(process.env.MONGO_URL); 
-    const users = await User.find()
+    const users = await User.find().select('-password -__v'); // exclude password and __v fields
+
     return Response.json(users)
 }
     //   const data = await getData();
