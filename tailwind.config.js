5213a1988d83d63f4c0e14bc7dbd66fc673a0134
Osagie <ituaosagie100@gmail.com>
2025-01-02 14:09:16 +0100

    .......................

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {nextui} from "@nextui-org/react";
+// import {nextui} from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -15,6 +15,6 @@ module.exports = {
      }
     },
   },
-  plugins: [nextui()],
+  plugins: [],
   darkMode: 'class',
 }
